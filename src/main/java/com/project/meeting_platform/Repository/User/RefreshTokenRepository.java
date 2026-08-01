c0d2c430a228fd5f032cdecd98e07974c16c9f00
lazaro <lazaroalves12355@gmail.com>
2026-08-01 12:34:42 -0300

    feat: adiciona atualização segura de credenciais

--- src/main/java/com/project/meeting_platform/Repository/User/RefreshTokenRepository.java
+++ src/main/java/com/project/meeting_platform/Repository/User/RefreshTokenRepository.java
@@ -4,10 +4,13 @@ import com.project.meeting_platform.Model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 public interface RefreshTokenRepository
         extends JpaRepository<RefreshToken, UUID> {
 
     Optional<RefreshToken> findByTokenHash(String tokenHash);
+
+    List<RefreshToken> findByUser_IdAndRevokedAtIsNull(UUID userId);
 }
